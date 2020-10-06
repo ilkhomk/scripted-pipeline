aad44236af68613b91413e12d65cb3c8ef1a5936
ilkhomk <i.kamolov1@gmail.com>
2020-10-06 02:10:12 +0000

    dock

---
+++ docker-flaskex.groovy
@@ -0,0 +1,21 @@
+node("docker"){
+    stage("Pull Repo"){
+        git url: 'https://github.com/ikambarov/Flaskex-docker.git'
+    }
+    
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', passwordVariable: 'REGISTRY_PASSWORD', usernameVariable: 'REGISTRY_USERNAME')]) {
+        stage("Docker Build"){
+            sh "docker build -t ${REGISTRY_USERNAME}/flaskex:v2  ."
+        }
+        
+        stage("Docker Login"){
+            sh "docker login -u ${REGISTRY_USERNAME} -p '${REGISTRY_PASSWORD}'"
+        }
+
+        stage("Docker Push"){
+            sh "docker push ${REGISTRY_USERNAME}/flaskex"
+        }
+    }
+
+    
+}
